0491c4b9c15871910d18ca42c5c771bcfb13e575
Mjanuary <janviermuhawenimana@gmail.com>
2019-12-01 18:36:46 +0200

    user connect and disconnect connection

--- server/server.js
+++ server/server.js
@@ -1,12 +1,29 @@
-const path = require('path');
-const express = require('express');
-
-const publicPath = path.join(__dirname, '/../public');
-const port = process.env.PORT || 3000;
+var express = require('express');
 var app = express();
 
-app.use(express.static(publicPath));
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var path = require('path');
 
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
-})
+var port = process.env.PORT || 3000;
+var publicPath = path.join(__dirname, '/../public');
+
+app.use(express.static(publicPath)); // serve the public files
+
+
+http.listen(port, function(){
+  console.log(`listening on *:${port}`);
+});
+
+
+io.on('connection', (socket) => {
+    console.log("A new user just connected");
+    
+    socket.on('disconnect', () => {
+        console.log('user was disonnected to the server');
+    });
+});
+
+// app.listen(port, () => {
+//     console.log(`Server is up on port ${port}`);
+// });
